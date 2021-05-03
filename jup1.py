532c6bd345f25be12e00f830b1f3c7cb76720501
PyCat <60161472+olgacot@users.noreply.github.com>
2021-05-02 22:40:30 -0400

    Add files via upload

---
+++ jup1.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# create dataframe with pandas
+dic = {"pizza_name":["Hawaii","Diablo","Pepperoni","Hawaii","Diablo","Pepperoni"],"company_name":["A","A","A","B","B","B"],"price":[5,7,9,8,12,14],"sale_2020":[1000,700,1400,800,300,600]}
+
+
+# In[2]:
+
+
+import pandas as pd
+df = pd.DataFrame(dic)
+
+
+# In[3]:
+
+
+df
+
+
+# In[ ]:
+
+
+
+
